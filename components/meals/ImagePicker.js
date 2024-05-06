dd9df1caec39389a32d1eb11f4c1ddeb6ed95cbf
KhatunaKhatuna <kh.chitishvili@gmail.com>
2024-05-06 14:10:26 +0400

    Hide image picker and create custom imagePicker button

---
+++ components/meals/ImagePicker.js
@@ -0,0 +1,32 @@
+"use client";
+import { useRef } from "react";
+import classes from "./ImagePicker.module.css";
+
+export default function ImagePicker({ label, name }) {
+  const imageInput = useRef();
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+        />
+        <button
+          onClick={handlePickClick}
+          className={classes.button}
+          type="button"
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
